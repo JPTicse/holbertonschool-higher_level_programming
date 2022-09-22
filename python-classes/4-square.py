53897e10dcd3e1a598a7eb92158b7189fa05512b
JPTicse <jpticse@gmail.com>
2022-09-21 20:01:12 -0500

    Change methods order

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,6 +8,9 @@ class Square:
     """Raise Error using conditionals"""
     def __init__(self, size=0):
         self.size = size
+    
+    def area(slef):
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -21,6 +24,3 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-
-    def area(self):
-        return self.__size * self.__size
