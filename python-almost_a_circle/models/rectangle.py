8f2848b0785a0d4331d76849139feeb99e80578e
JPTicse <jpticse@gmail.com>
2022-10-05 07:51:54 -0500

    Fixing typos in TypeError

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,9 +28,9 @@ class Rectangle(Base):
     def width(self, value):
         """ width setter """
         if type(value) is not int:
-            raise TypeError ("width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError ("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -42,9 +42,9 @@ class Rectangle(Base):
     def height(self, value):
         """ width setter """
         if type(value) is not int:
-            raise TypeError ("height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError ("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -56,9 +56,9 @@ class Rectangle(Base):
     def x(self, value):
         """ x setter """
         if type(value) is not int:
-            raise TypeError ("x must be an integer")
+            raise TypeError("x must be an integer")
         if value <= 0:
-            raise ValueError ("x must be > 0")
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -70,7 +70,7 @@ class Rectangle(Base):
     def y(self, value):
         """ y setter """
         if type(value) is not int:
-            raise TypeError ("y must be an integer")
+            raise TypeError("y must be an integer")
         if value <= 0:
-            raise ValueError ("y must be > 0")
+            raise ValueError("y must be > 0")
         self.__y = value
