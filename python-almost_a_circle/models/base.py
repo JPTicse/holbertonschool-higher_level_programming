ba223753785787ab20c4a84a3669e65b225292b7
JPTicse <jpticse@gmail.com>
2022-10-04 21:51:22 -0500

    Adding folder tests and Task 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+""" Module that contains class Base """
+
+from multiprocessing.sharedctypes import Value
+from time import sleep
+
+
+class Base:
+    """ Class Base """
+    __nbobjects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nbobjects += 1
+            self.id = Base.__nbobjects
