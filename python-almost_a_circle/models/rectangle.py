22572c15e026b29d20ad049dc56f067d12d0f5f7
JPTicse <jpticse@gmail.com>
2022-10-05 07:58:08 -0500

    Fixyng PycodeStyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that contains class Rectangle 
+""" Module that contains class Rectangle
 inheritance of class Base
 """
 
@@ -9,7 +9,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """ Class Rectangle """
-
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes instances """
