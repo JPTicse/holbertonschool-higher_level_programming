4dc4843a714a011faf8b683c7e77e14f75208e52
JPTicse <jpticse@gmail.com>
2022-10-05 07:46:56 -0500

    Fixing confitions in setters and fix Typerror

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,8 +28,8 @@ class Rectangle(Base):
     def width(self, value):
         """ width setter """
         if type(value) is not int:
-            raise TypeError ("width must be integer")
-        if value < 0:
+            raise TypeError ("width must be an integer")
+        if value <= 0:
             raise ValueError ("width must be > 0")
         self.__width = value
 
@@ -42,8 +42,8 @@ class Rectangle(Base):
     def height(self, value):
         """ width setter """
         if type(value) is not int:
-            raise TypeError ("height must be integer")
-        if value < 0:
+            raise TypeError ("height must be an integer")
+        if value <= 0:
             raise ValueError ("height must be > 0")
         self.__height = value
 
@@ -56,8 +56,8 @@ class Rectangle(Base):
     def x(self, value):
         """ x setter """
         if type(value) is not int:
-            raise TypeError ("x must be integer")
-        if value < 0:
+            raise TypeError ("x must be an integer")
+        if value <= 0:
             raise ValueError ("x must be > 0")
         self.__x = value
 
@@ -70,7 +70,7 @@ class Rectangle(Base):
     def y(self, value):
         """ y setter """
         if type(value) is not int:
-            raise TypeError ("y must be integer")
-        if value < 0:
+            raise TypeError ("y must be an integer")
+        if value <= 0:
             raise ValueError ("y must be > 0")
         self.__y = value
