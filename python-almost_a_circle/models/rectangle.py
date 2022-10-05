6041259466d311c7c14b60415c3217df5955ccb0
JPTicse <jpticse@gmail.com>
2022-10-04 23:06:59 -0500

    Fixing name class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,8 @@
 from models.base import Base
 
 
-class Rectangl(Base):
+class Rectangle(Base):
     """ Class Rectangle """
-    __nbobjects = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes instances """
