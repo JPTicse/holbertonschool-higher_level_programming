4e74f0904e64cbb7a7166c03cc3e3ed23d4cd306
JPTicse <jpticse@gmail.com>
2022-10-06 10:02:24 -0500

    Add getter and setter to size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 inheritance of class Rectangle
 """
 
+from re import S
 from models.rectangle import Rectangle
 
 
@@ -10,7 +11,9 @@ class Square(Rectangle):
     """ Class inherit from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Init instances"""
+        self.size = size
         super().__init__(size, size, x, y, id)
+        
 
     def __str__(self):
         """ str dunder method """
@@ -20,3 +23,14 @@ class Square(Rectangle):
         str_wh = "{}".format(self.width, self.width)
 
         return str_rectangle + str_id + str_xy + str_wh
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
